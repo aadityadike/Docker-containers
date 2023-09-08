2c577aee89adefdd71d8f1c986f18fd24aa69d6a
aadityadike <adityadike123@gmail.com>
2023-09-08 17:41:53 +0530

    NODE API IS DONE

--- Node-Application/src/db.js
+++ Node-Application/src/db.js
@@ -2,13 +2,10 @@ const fs = require("fs");
 const { Pool } = require("pg");
 
 // If 'DATABASE_URL' is not defined, read the content of the file specified in 'DATABASE_URL_FILE'.
-databaseURL = process.env.DATABASE_URL;
+databaseURL = process.env.DATABASE_URL ||'postgres://postgres:adityaspostgres@db:5432/dockerPractice'
 
 const pool = new Pool({
-  user: "aditya",
-  database: "DockerPractice",
-  password: "adityasdatabase",
-  port: 5432,
+  connectionString: databaseURL ,
 });
 
 /*
@@ -26,7 +23,7 @@ const getDateTime = async () => {
     const res = await client.query("SELECT NOW() as now;");
     return res.rows[0];
   } catch (err) {
-    console.log(err.stack);
+    console.log("Error in getting time", err.stack);
   } finally {
     client.release();
   }
